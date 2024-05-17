a7166ddc84c62fe2d30522d82c0b6b0e5a9f1936
apichatpunya <apichat.punya@gmail.com>
2024-05-17 10:01:05 +0700

    update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,17 +11,32 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text("My APP"),
       ),
       body: Row(
-          children: [
-            Column(),
-            Column(),
-            Column()
-          ],
-      ) ,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            
+            children: [
+            Icon(Icons.call, color: Color.fromARGB(255, 6, 88, 239)),
+            Text("Call"),
+          ]),
+          Column(
+            children: [
+              Icon(Icons.camera, color: Color.fromARGB(255, 6, 88, 239)),
+            Text("Camera"),
+            ]
+          ),
+          Column(
+            children: [
+              Icon(Icons.share, color: Color.fromARGB(255, 6, 88, 239)),
+            Text("Share"),
+            ]
+          )
+        ],
+      ),
     );
   }
 }
